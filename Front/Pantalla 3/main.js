af382a5a6ce4406b7df504d58b6d8e81182f2e24
camilazamenfeld <camizamenfeld@gmail.com>
2025-10-09 12:04:18 -0300

    Update main.js

---
+++ Front/Pantalla 3/main.js
@@ -0,0 +1,7 @@
+document.getElementById("volverPantalla1").addEventListener("click", function() {
+    window.location.href = "../Pantalla 1/index.html";
+  });
+  document.getElementById("comenzar").addEventListener("click", function() {
+    window.location.href = "../Pantalla 4/index.html";
+  });
+  
