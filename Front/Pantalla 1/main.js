3d0d818aa8b2662b2dcf35c0a3ca0ecc7db37180
camilazamenfeld <camizamenfeld@gmail.com>
2025-10-09 12:04:48 -0300

    Update main.js

---
+++ Front/Pantalla 1/main.js
@@ -0,0 +1,7 @@
+document.getElementById("irPantalla2").addEventListener("click", function() {
+    window.location.href = "../Pantalla 2/index.html";
+  });
+  document.getElementById("registrarme").addEventListener("click", function() {
+    window.location.href = "../Pantalla 3/index.html";
+  });
+  
