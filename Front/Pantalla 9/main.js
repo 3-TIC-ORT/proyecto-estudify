454d3da4c23886089b387a23ee10495220d9fd41
camilazamenfeld <camizamenfeld@gmail.com>
2025-10-16 12:02:32 -0300

    Update main.js

---
+++ Front/Pantalla 9/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const profesores = document.querySelectorAll('.profesor');
+  
+    profesores.forEach(profesor => {
+      profesor.addEventListener('click', () => {
+        window.location.href = '../Pantalla 6/index.html';
+      });
+    });
+  });
+  
