dda071fbd75a98cd13e0c9fc04557ba851fe6a9f
juanamarinansky <juanamarinansky1@gmail.com>
2025-10-21 08:51:38 -0300

    Update main.js

--- Back/main.js
+++ Back/main.js
@@ -7,3 +7,14 @@ startServer(3000);
 // sign up
 
 subscribePOSTEvent('registrarUsuario', registrarUsuario);
+
+// lo que estaba antes
+let data = fs.readFileSync("usuarios.json", "utf-8");
+
+let usuarios = JSON.parse(data);
+
+usuarios.push({"usuario": "Luu", "contra": "1234"});
+
+let nuevoJson = JSON.stringify(usuarios, null, 2);
+
+fs.writeFileSync("usuarios.json", nuevoJson);
