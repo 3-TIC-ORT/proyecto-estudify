4c98014bb2cea9f510a4d072cf2d32408ede3931
camilazamenfeld <camizamenfeld@gmail.com>
2025-10-22 10:34:57 -0300

    hice que los iconos de usuario y mensajes funciones y que cuando cliqueas un mensaje podes escribirle

--- Front/Pantalla 7/main.js
+++ Front/Pantalla 7/main.js
@@ -1,8 +1,31 @@
 
-document.querySelectorAll('.mensaje').forEach(mensaje => {
+document.addEventListener('DOMContentLoaded', () => {
+  const mensajes = document.querySelectorAll('.mensaje');
+
+  mensajes.forEach(mensaje => {
+
+    mensaje.style.cursor = 'pointer';
+
     mensaje.addEventListener('click', () => {
-      const profesor = mensaje.dataset.profesor;
-      window.location.href = "../Pantalla 8/index.html?profesor=" + profesor;
+      const profesor = mensaje.dataset.profesor || 'desconocido';
+      console.log('Clic en mensaje:', profesor); 
+
+  
+      window.location.href = "../Pantalla 8/index.html?profesor=" + encodeURIComponent(profesor);
+
+    
     });
   });
-  
+});
+if (iconoUsuario) {
+  iconoUsuario.addEventListener("click", () => {
+    window.location.href = "../Pantalla 11/index.html"; 
+  });
+}
+
+const iconoMensajes = document.querySelector(".mensajes");
+if (iconoMensajes) {
+  iconoMensajes.addEventListener("click", () => {
+    window.location.href = "../Pantalla 7/index.html"; 
+  });
+}
